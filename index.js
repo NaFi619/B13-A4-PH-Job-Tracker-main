45293b4acf6f7cc68c88d708c8c13528e023278b
NaFi619 <adruha000@gmail.com>
2026-03-06 16:51:42 +0600

    done

--- index.js
+++ index.js
@@ -9,7 +9,27 @@ const interviewContainer = document.getElementById("interview-container");
 const rejectedContainer = document.getElementById("rejected-container");
 
 
+function checkEmptyState() {
+    const noJobContainer = document.getElementById("no-job-container");
+    let activeContainer;
+
+    if (currentTab === "all") {
+        activeContainer = allContainer;
+    } else if (currentTab === "interview") {
+        activeContainer = interviewContainer;
+    } else if (currentTab === "rejected") {
+        activeContainer = rejectedContainer;
+    }
+    if (activeContainer && activeContainer.children.length === 0) {
+        noJobContainer.classList.remove("hidden");
+    } else {
+        noJobContainer.classList.add("hidden");
+    }
+}
+
+
 function openTab(tabName) {
+    currentTab = tabName;
     const tabs = ["all", "interview", "rejected"];
     for(let tab of tabs) {
         if(tab === tabName) {
@@ -36,6 +56,7 @@ function openTab(tabName) {
         interviewContainer.classList.add("hidden");
         rejectedContainer.classList.remove("hidden");
     }
+    checkEmptyState();
 }
 
 
@@ -44,12 +65,18 @@ function updateDashboardCounts() {
     const allCount = document.getElementById("all-container").children.length;
     const interviewCount = document.getElementById("interview-container").children.length;
     const rejectedCount = document.getElementById("rejected-container").children.length;
+    const totalTextCount = document.getElementById("total-job-count-dashboard-count");
+    
+    
+    const totalSum = allCount + interviewCount + rejectedCount;
 
-    document.getElementById("total-dashboard-count").innerText = allCount + interviewCount + rejectedCount;
+    document.getElementById("total-dashboard-count").innerText = totalSum;
     document.getElementById("interview-dashboard-count").innerText = interviewCount;
     document.getElementById("rejected-dashboard-count").innerText = rejectedCount;
+    
+    totalTextCount.innerText = totalSum;
+    
 }
-
 
 allContainer.addEventListener("click", function(event) {
     const interviewBtn = event.target.closest(".interview");
@@ -58,24 +85,30 @@ allContainer.addEventListener("click", function(event) {
     const card = event.target.closest(".card");
 
     const status = card.querySelector(".status");
+
+    const noJobThing = document.getElementById("no-job-container");
     
     if (interviewBtn) {
-        status.innerText = "Interview";
-        status.className = "mt-2 mb-2 btn btn-neutral font-semibold text-sm status whitespace-nowrap w-fit"; 
+        status.innerText = "Interviewed";
+        status.className = "mt-2 mb-2 btn btn-success rounded-full font-semibold text-sm status whitespace-nowrap w-fit"; 
         document.getElementById("interview-container").appendChild(card);
         updateDashboardCounts();
+        checkEmptyState();
     } 
     else if (rejectedBtn) {
         status.innerText = "Rejected";
-        status.className = "mt-2 mb-2 btn btn-neutral font-semibold text-sm status whitespace-nowrap w-fit";
+        status.className = "mt-2 mb-2 btn btn-error rounded-full font-semibold text-sm status whitespace-nowrap w-fit";
         document.getElementById("rejected-container").appendChild(card);
         updateDashboardCounts();
+        checkEmptyState();
     } 
     else if (deleteBtn) {
         card.remove(); 
         updateDashboardCounts();
+        checkEmptyState();
     }
 });
 
 openTab(currentTab);
+updateDashboardCounts();
 
